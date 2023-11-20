904a08543297b372f824e220a57c36f5ee26f85e
BigBird15 <justabigbird@gmail.com>
2023-11-21 02:32:09 +0500

    inhancing players fixed

--- src/helper.js
+++ src/helper.js
@@ -4,7 +4,7 @@ const GRID_GAP = 10;
 const viewportHeight = window.innerHeight;
 
 export const getWidth = function () {
-    let itemWidth = window.innerWidth;
+    let itemWidth;
 
     return function (count) {
         const itemsPerRow = Math.floor(window.innerWidth / itemWidth) || 1;
@@ -12,13 +12,13 @@ export const getWidth = function () {
         const itemHeight = itemWidth / ASPECT_RATIO;
         const heightTakenByItems = rowsCount * (itemHeight + GRID_GAP);
         const lackOfHeight = heightTakenByItems - viewportHeight;
-        const heightProportion = viewportHeight / (viewportHeight + lackOfHeight);
+        const resizingCoefficient = viewportHeight / (viewportHeight + lackOfHeight);
 
-        if (heightTakenByItems > window.innerHeight) {
-            itemWidth = ASPECT_RATIO * (heightProportion * heightTakenByItems / rowsCount) + GRID_GAP;
-        } else if (itemHeight * rowsCount < Math.abs(lackOfHeight)) {
-            //
+        const itemResizingRequired = heightTakenByItems > viewportHeight || Math.abs(lackOfHeight) > itemHeight + GRID_GAP;
+        if (itemResizingRequired) {
+            itemWidth = ASPECT_RATIO * (resizingCoefficient * heightTakenByItems / rowsCount) + GRID_GAP;
         }
+        if (count === 0 || count === 1) itemWidth = window.innerWidth - 2 * GRID_GAP
         return itemWidth;
     }
 }
