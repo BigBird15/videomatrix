f8a42d9279a5dcb18799091732a5916fd08fd185
BigBird15 <justabigbird@gmail.com>
2023-11-21 21:25:27 +0500

    Revert "fixed cannot read properties of null (reading "load") error"
    This reverts commit ac1fb6a89d091cd73e25b8d0fe1d6224894bdf78.

--- src/PlayersSelectMenu.js
+++ src/PlayersSelectMenu.js
@@ -12,8 +12,9 @@ const PlayersSelectMenu = props => {
         if (event.target.checked) {
             dispatch(addSource(source));
         } else {
+            // TODO errors `cannot read properties of null (reading "load")` happen sometimes
             const playerId = getSafeString(source);
-            videojs(playerId).reset();
+            videojs(playerId).dispose();
             dispatch(removeSource(source));
         }
     };
